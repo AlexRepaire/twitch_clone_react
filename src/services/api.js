18c213bc68d2c32ee40bd64056c5ba522b7fc936
AlexRepaire <56023123+AlexRepaire@users.noreply.github.com>
2021-11-17 11:19:49 +0100

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -6,12 +6,5 @@ let api = axios.create({
         "Authorization": "Bearer secret",
     }
 })
-/*
-    CLIENT_ID = '6vz1fcbuxqqcbw7j1g40g3nu8oyqvd';
-    REDIRECT = 'http://127.0.0.1/';
 
-    LIEN AUTH = https://id.twitch.tv/oauth2/authorize?client_id=6vz1fcbuxqqcbw7j1g40g3nu8oyqvd&redirect_uri=https://127.0.0.1/&response_type=token
-
-
-*/
 export default api;
